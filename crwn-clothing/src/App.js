ddb8ed199b89d87aae98e8dc57df1393b735c169
Ryan Sargeson <ryanteachviet@gmail.com>
2022-01-06 17:09:34 +0000

    second commit

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './HomePage.component';
+import HomePage from './page/HomePage/HomePage.component';
 
 function App() {
   return (
---
+++ crwn-clothing/src/components/menu-item/menu-item.components.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import './menu-item.styles.scss';
+
+const MenuItem = ({title, image}) => {
+    return(
+    <div style={{
+        backgroundImage: `url(${image})`
+    }}
+    
+    className={`{$size} Menu-item`}>
+        <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subtitle">Shop Now</span>
+        </div>
+    </div>
+    );
+}
+
+export default MenuItem;
