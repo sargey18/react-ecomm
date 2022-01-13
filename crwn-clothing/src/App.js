953ee806b5ac757ddce680585c40bc8d637316c5
Ryan Sargeson <ryanteachviet@gmail.com>
2022-01-13 18:48:44 +0000

    added the header and the basic lins with logo

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,12 +3,14 @@ import {Switch, Route} from 'react-router-dom'
 import './App.css';
 import HomePage from './page/HomePage/HomePage.component';
 import ShopPage from './page/shop/shop.component';
+import Header from './components/header/header.component.jsx';
 
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route  path='/shop' component={ShopPage}/>
