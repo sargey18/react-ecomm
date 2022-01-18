a0fa3bc774e145e43c86599ffc5272879cc231d8
Ryan Sargeson <ryanteachviet@gmail.com>
2022-01-18 12:04:07 +0000

    added the google signup

--- crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
@@ -36,6 +38,8 @@ class SignIn extends React.Component{
                     <FormInput name="password" type="password" value={this.state.password} label='password' handleChange={this.handleChange} required/>
 
                     <CustomButton type='submit'>Sign In</CustomButton>
+                    <CustomButton onClick={signInWithGoogle}>Sign In with Google</CustomButton>
+
                 </form>
             </div>
         )
---
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+
+const config = {
+    apiKey: "AIzaSyAtr2aWhKGACeg7EKzOdyDfxoyv-Ca3rF0",
+    authDomain: "crwn-clothing-8460f.firebaseapp.com",
+    projectId: "crwn-clothing-8460f",
+    storageBucket: "crwn-clothing-8460f.appspot.com",
+    messagingSenderId: "206228643652",
+    appId: "1:206228643652:web:d1bb6158f7804b1d37c111",
+    measurementId: "G-0JRH9B4PNM"
+};
+
+firebase.initializeApp(config);
+
+export const auth =firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
