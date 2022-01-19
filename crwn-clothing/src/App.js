178862cfb43d1ea33b916042e22d0a73491060aa
Ryan Sargeson <ryanteachviet@gmail.com>
2022-01-19 16:20:42 +0000

    started to connect the database by storing the user

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -5,7 +5,7 @@ import HomePage from './page/HomePage/HomePage.component';
 import ShopPage from './page/shop/shop.component';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from './page/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUsrProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor(){
@@ -19,8 +19,9 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      //this.setState({currentUser: user});
+      createUsrProfileDocument(user);
 
       console.log(user);
     })
