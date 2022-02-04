8a1171f288cbf90d908474914fcb7dfa230a44b3
Ryan Sargeson <ryanteachviet@gmail.com>
2022-02-04 12:53:28 +0000

    added the initial reducer

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
