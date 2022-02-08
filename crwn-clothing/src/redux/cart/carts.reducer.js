7c0a76f3f51a59fc53abc868276686fe8b64351d
Ryan Sargeson <ryanteachviet@gmail.com>
2022-02-08 18:04:04 +0000

    added the toggle for the cart drop down

---
+++ crwn-clothing/src/redux/cart/carts.reducer.js
@@ -0,0 +1,24 @@
+
+import CartActionTypes from './cart.types.js';
+
+const INITIAL_STATE = {
+    hidden: true 
+}
+// first make the initial state 
+// second make a fundtion with the state set as initial state and an action
+//the switch has the action type 'TOGGLE_CART_HIDDEN'
+//if this is there then spread the state with hidden with not state hidden
+//else return default state 
+const cartReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case CartActionTypes.TOGGLE_CART_HIDDEN:
+            return {
+                ...state,
+                hidden: !state.hidden
+            }
+            default:
+                return state;
+    }
+}
+
+export default cartReducer;
