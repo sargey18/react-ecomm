4d869bf732eeefb58afaaf754d2f22d956b43648
Ryan Sargeson <ryanteachviet@gmail.com>
2022-02-09 12:00:14 +0000

    we an now add multiple items to the cart

--- crwn-clothing/src/redux/cart/carts.reducer.js
+++ crwn-clothing/src/redux/cart/carts.reducer.js
@@ -1,5 +1,6 @@
 
 import CartActionTypes from './cart.types.js';
+import { addItemToCart } from './cart.utils.js';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -20,7 +21,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             case CartActionTypes.ADD_ITEM:
                 return{
                     ...state,
-                    cartItems: [...state.cartItems, action.payload]
+                    cartItems: addItemToCart(state.cartItems, action.payload)
                 };
             default:
                 return state;
