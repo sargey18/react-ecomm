1c50b33953be27b94bf7167355ce289572f69690
Ryan Sargeson <ryanteachviet@gmail.com>
2022-02-09 19:48:49 +0000

    added the checkout page

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -7,6 +7,7 @@ import HomePage from './page/HomePage/HomePage.component';
 import ShopPage from './page/shop/shop.component';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from './page/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './page/checkout/checkout.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import {setCurrentUser} from './redux/user/user.actions';
 import {selectCurrentUser} from './redux/user/user.selectors';
@@ -50,6 +51,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route  path='/shop' component={ShopPage}/>
+          <Route exact path='/checkout' component={CheckoutPage}/>
+
           <Route exact  path='/signin' render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)}/>
         </Switch>
       </div>
